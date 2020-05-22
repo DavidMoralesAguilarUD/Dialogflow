5a654074c39c320bade216e43cc8e1fe67c512e6
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2020-05-22 17:48:33 -0500

    Giving back simple Responses

---
+++ functions/DialogLib.js
@@ -0,0 +1,32 @@
+function respuestaBasica(textoEnviar) {
+    let respuesta = {
+        "fulfillmentText": textoEnviar,
+        "fulfillmentMessages": [
+            {
+                "platform": "ACTIONS_ON_GOOGLE",
+                "simpleResponses": {
+                    "simpleResponses": [
+                        {
+                            "textToSpeech": textoEnviar
+                        }
+                    ]
+                }
+            },
+            {
+                "text": {
+                    "text": [
+                        textoEnviar
+                    ]
+                }
+            }
+        ]
+    }
+    return respuesta;
+}
+function hola(nombre) {
+    console.log("Encantado de conocerte " + nombre);
+}
+module.exports = {
+    hola: hola,
+    respuestaBasica: respuestaBasica
+}
